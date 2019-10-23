3dd97bf7d61cb0e7d0c65d7c06efdd15e4e14676
Pyrnapp <zcienceguy@gmail.com>
2019-10-23 19:53:19 -0400

    Continued Setup

--- src/main/java/com/pyrnapp/TidesoftheDream/InitialTesting.java
+++ src/main/java/com/pyrnapp/TidesoftheDream/InitialTesting.java
@@ -28,12 +28,12 @@ public class InitialTesting {
     public InitialTesting() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        //// Register the enqueueIMC method for modloading
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        //// Register the processIMC method for modloading
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        //// Register the doClientStuff method for modloading
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -45,25 +45,25 @@ public class InitialTesting {
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
-    }
+    //private void doClientStuff(final FMLClientSetupEvent event) {
+    //    // do something that can only be done on the client
+    //    LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    //}
 
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> {
-            LOGGER.info("Hello world from the MDK");
-            return "Hello world";
-        });
-    }
+    //private void enqueueIMC(final InterModEnqueueEvent event) {
+    //    // some example code to dispatch IMC to another mod
+    //    InterModComms.sendTo("examplemod", "helloworld", () -> {
+    //        LOGGER.info("Hello world from the MDK");
+    //        return "Hello world";
+    //    });
+    //}
 
-    private void processIMC(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m -> m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
+    //private void processIMC(final InterModProcessEvent event) {
+    //    // some example code to receive and process InterModComms from other mods
+    //    LOGGER.info("Got IMC {}", event.getIMCStream().
+    //            map(m -> m.getMessageSupplier().get()).
+    //            collect(Collectors.toList()));
+    //}
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
